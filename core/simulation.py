df6d18adea5570d120c1971a7a4032fdbb9b4d3c
kaijaz123 <yew815@outlook.com>
2022-01-12 21:49:51 +0800

    update display demo pattern

--- core/simulation.py
+++ core/simulation.py
@@ -2,12 +2,21 @@ import cv2
 import numpy as np
 
 def display_demo_pattern(demo, ptns):
+    # font settings
+    height, width = demo.shape[:2]
+    resolution = height+width
+    pos_x = int(resolution * 0.01)
+    pos_y = int(resolution * 0.03)
+    font_size = 0.5 * (resolution) / 600
+    font_scale = int(0.6 * (resolution) // 300)
+    font_style = cv2.FONT_HERSHEY_DUPLEX
+
     # display pattern
-    cv2.putText(demo, "Pattern: ", (10,25), cv2.FONT_HERSHEY_SIMPLEX, 0.8 , (255,255,255), 2)
-    x_axis = 115
+    cv2.putText(demo, "Pattern: ", (pos_x,pos_y), font_style, font_size, (255,255,255), font_scale)
+    x_axis = (resolution)//8 # pattern result pos
     for ptn in ptns:
-        cv2.putText(demo, str(ptn[0]), (x_axis,28), cv2.FONT_HERSHEY_SIMPLEX, 0.9, ptn[1], 2)
-        x_axis += 25
+        cv2.putText(demo, str(ptn[0]), (int(x_axis),pos_y+int(pos_y*0.06)), font_style, font_size, ptn[1], font_scale)
+        x_axis += (resolution)//50 # next pattern result pos
 
     return demo
 
@@ -30,6 +39,7 @@ def display_demo_ball(demo, ball):
     for index,_ in enumerate(traces):
         if index == 0: continue
         thickness = index + 2
-        cv2.line(demo, tuple(np.array(traces[index - 1]).astype(int)), tuple(np.array(traces[index]).astype(int)), ball["colors"], thickness)
+        cv2.line(demo, tuple(np.array(traces[index - 1]).astype(int)), tuple(np.array(traces[index]).astype(int)),
+                 ball["colors"], thickness)
 
     return demo
